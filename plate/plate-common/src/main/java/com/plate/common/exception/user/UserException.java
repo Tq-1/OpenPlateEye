6797e0f6619c82f338571b7fdbb71b9626abb744
Tq-1 <2046766443@qq.com>
2024-12-21 22:08:09 +0800

    Update Project3

---
+++ plate/plate-common/src/main/java/com/plate/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.plate.common.exception.user;
+
+import com.plate.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
---
+++ plate/plate-common/src/main/java/com/plate/common/test/test.java
@@ -0,0 +1,9 @@
+package com.plate.common.test;
+
+import com.plate.common.utils.SecurityUtils;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(SecurityUtils.encryptPassword("Tyadmin20020303"));
+    }
+}
